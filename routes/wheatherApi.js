b48e2dc6bec97c5891f7ceb7db8a139e27c8d5e0
newasad <asadhmeed2@gmail.com>
2024-01-07 15:34:57 +0200

    server add get delete is working

--- routes/wheatherApi.js
+++ routes/wheatherApi.js
@@ -4,7 +4,6 @@ const router = express.Router()
 const wheatherApiModule = require('../externalAPI/extWhetherAPI')
 
 const Wether = require('../models/Wether')
-const { ExtWeatherApiError } = require('../customErrors/customErrors')
 
 router.get('/', function (req, res) {
     Wether.find({}).then( function (wheathers) {
@@ -27,7 +26,6 @@ router.get('/search', async function (req, res) {
 router.post('/', function (req, res) {
     const wether = req.body
     try{
-
         Wether.create(wether).then( function (data){
             console.log(data);
             res.status(201).json(data)
@@ -35,19 +33,6 @@ router.post('/', function (req, res) {
     }catch(error){
         console.error(error);
     }
-})
-
-router.put('/:id', function (req, res) {
-    const wetherId = req.params.id;
-    const wether = req.body
-    
-        Wether.findByIdAndUpdate(wetherId,wether,{new:true}).then( function (data){
-            console.log(data);
-            res.status(201).json(data)
-        }).catch(error=>{
-            console.error(error);
-        })
-    
 })
 
 router.delete ('/:id', function (req, res) {
